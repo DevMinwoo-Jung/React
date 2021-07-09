d79d6019adc03a59d9e9560e67a2159f1439f701
정민우/MinWoo Jung <applejung312@gmail.com>
2021-07-09 15:36:36 +0900

    Initial

---
+++ react-basic/habit-tracker/.history/src/index_20210709145013.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // root에 app componet를 연결!
+  // stricMode는 javascript의 use strict와 비슷한거이당 
+  document.getElementById('root')
+);
+
