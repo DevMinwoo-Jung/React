9ff28809c0555444596dbbcf7714d17c49a37395
Minwoo <applejung312@gmail.com>
2021-09-18 11:00:04 +0900

    20210918

--- financial-ledger/src/components/date_search_form/date_search_form.jsx
+++ financial-ledger/src/components/date_search_form/date_search_form.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import styles from './date_search_form.module.css';
+
 
 const DateSearchForm = ({onSubmit, startRef, endRef}) => {
   return (
-    <div>
-      <form>
+    <div className={styles.search__div}>
+      <form className={styles.search__form}>
         <input ref={startRef} type="date"  />
         <input ref={endRef} type="date" />
+        <button onClick={onSubmit}>클릭</button>
       </form>
-      <button onClick={onSubmit}>아 눌러봐요</button>
     </div>
   );
 };
